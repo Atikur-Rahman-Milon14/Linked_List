65ddf43a6bcf2f00ce7da44c244fd61d5b18280b
Atikur Rahman Milon <milon14490@gmail.com>
2026-07-07 00:03:21 +0600

    Take input from user

---
+++ Take_input_from_user.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node* &head, Node* &Tail,int val){
+    Node* newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        Tail = newNode;
+        return;
+    }
+    Tail->next = newNode;
+    Tail = newNode;
+};
+void print_tail(Node* head){
+    Node* tmp = head;
+    while(tmp != NULL){
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+};
+int main(){
+    Node* head = NULL;
+    Node* Tail = NULL;
+    int N;
+    while(true){
+        cin >> N;
+        if(N == -1){
+            break;
+        }
+
+        insert_at_tail(head,Tail,N);
+    }
+    print_tail(head);
+    return 0;
+}
