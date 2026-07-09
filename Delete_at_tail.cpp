5b2ec70433467c8bf501c59097a9ca81fa666797
Atikur Rahman Milon <milon14490@gmail.com>
2026-07-09 19:55:08 +0600

    Delete at Tail

---
+++ Delete_at_tail.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node* &head, Node* &Tail, int N){
+    Node* newNode = new Node(N);
+    if(head == NULL){
+        head = newNode;
+        Tail = newNode;
+        return;
+    }
+    Tail->next = newNode;
+    Tail = newNode;
+}
+void print_node(Node* head){
+    Node* tmp = head;
+    while(tmp != NULL){
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+}
+
+void delet_at_taill(Node* &head,Node* &Tail, int idx){
+    if(idx == 0){
+        Node* deleteNode = head;
+        head = head->next;
+        delete deleteNode;
+        return;
+    }
+
+    Node* tmp = head;
+    for(int i = 1; i< idx; i++){
+        tmp = tmp->next;
+    }
+
+    Node* deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    delete deleteNode;
+    Tail = tmp;
+    
+}
+
+int main(){
+    Node* head = NULL;
+    Node* Tail = NULL; 
+    int N;
+    while(true){
+        cin >> N;
+        if(N == -1){
+            break;
+        }
+        else{
+            insert_at_tail(head,Tail,N);
+        }
+    }
+    delet_at_taill(head,Tail, 6);
+    print_node(head);
+    return 0;
+}
